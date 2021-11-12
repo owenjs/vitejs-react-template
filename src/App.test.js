3c9513bb213393e744eccdaa63325fdc47498d6a
Owen Evans <owen@3sidedcube.com>
2021-11-12 11:48:54 +0000

    react tests added to project

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import App from "src/App";
+
+describe("App", () => {
+  test("should render Learn React link", () => {
+    render(<App />);
+
+    expect(screen.getByRole("link", { name: "Learn React" })).toBeDefined();
+  });
+});
